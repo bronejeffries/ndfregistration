0b29cc751121cf8c74f938840c0f33e9088a7ea7
Your Name <you@example.com>
2019-10-25 12:36:25 +0300

    remove changes

--- app/Participant.php
+++ app/Participant.php
@@ -3,10 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Http\Traits\Hashidable;
 
 class Participant extends Model
 {
     //
+
+    use Hashidable;
 
     protected $guarded = ['id'], $attributes = [
         'payment_reference'=>0
@@ -16,5 +19,13 @@ class Participant extends Model
     {
         return $this->belongsTo(Ekisakaate::class,'ekn_id');
     }
+
+    public function resolveRouteBinding($value)
+    {
+        $id = Hashids::connection(get_called_class())->decode($value)[0] ?? null;
+        return $this->where('id', $id)->first() ?? abort(404);
+
+    }
+
 
 }
